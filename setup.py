79806147c873dfce342baaa932ee4e62800c5557
Joaquim Rocha <jjmrocha@gmail.com>
2020-12-06 18:15:22 +0000

    update setup

--- setup.py
+++ setup.py
@@ -13,6 +13,12 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/jjmrocha/fd-crawler",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'selenium',
+        'requests',
+        'python-dateutil',
+        'pytz'
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
